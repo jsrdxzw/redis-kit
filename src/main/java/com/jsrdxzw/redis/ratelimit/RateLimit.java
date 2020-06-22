4d17ec9fa6dd80142ea308a318c294d31b4f9cfa
jsrdxzw <jsrdxzw@163.com>
2020-06-22 22:53:55 +0800

    rate limit

---
+++ src/main/java/com/jsrdxzw/redis/ratelimit/RateLimit.java
@@ -0,0 +1,11 @@
+package com.jsrdxzw.redis.ratelimit;
+
+/**
+ * @author xuzhiwei
+ * @date 2020/06/22
+ * implement three algorithm
+ * including rolling window token bucket and leaky bucket
+ */
+public interface RateLimit {
+
+}
